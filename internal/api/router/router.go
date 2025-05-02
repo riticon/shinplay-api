c361d710145c931e643d48d2c73c75fa698e82b3
Pavan Kumar V <66193049+pavankumar-v@users.noreply.github.com>
2025-05-02 16:03:59 +0530

    chore(server):  integrate zap logger for requests

--- internal/api/router/router.go
+++ internal/api/router/router.go
@@ -1,16 +1,17 @@
 package router
 
 import (
-	"log"
 	"time"
 
+	"github.com/gofiber/contrib/fiberzap/v2"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/compress"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/helmet"
 	"github.com/gofiber/fiber/v2/middleware/limiter"
-	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/fiber/v2/middleware/recover"
 	"github.com/gofiber/fiber/v2/middleware/requestid"
+	"go.uber.org/zap"
 
 	"github.com/shinplay/internal/config"
 )
@@ -24,20 +25,19 @@ func CreateNewFiberApp(config *config.Config) *fiber.App {
 
 	app.Use(recover.New())
 	app.Use(requestid.New())
-	app.Use(logger.New(logger.Config{
-		Format:     "${time} ${ip} ${status} - ${method} ${path} ${latency}\n",
-		TimeFormat: "2006-01-02 15:04:05",
-		TimeZone:   "Local",
+
+	app.Use(fiberzap.New(fiberzap.Config{
+		Logger: config.Logger,
 	}))
 
-	// app.Use(cors.New(cors.Config{ // CORS configuration
-	// 	AllowOrigins:     config.Env.CORS,
-	// 	AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS",
-	// 	AllowHeaders:     "Origin, Content-Type, Accept, Authorization, X-Request-ID",
-	// 	ExposeHeaders:    "Content-Length, Content-Type",
-	// 	AllowCredentials: true,
-	// 	MaxAge:           86400, // 24 hours
-	// }))
+	app.Use(cors.New(cors.Config{ // CORS configuration
+		AllowOrigins:     config.Env.CORS,
+		AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS",
+		AllowHeaders:     "Origin, Content-Type, Accept, Authorization, X-Request-ID",
+		ExposeHeaders:    "Content-Length, Content-Type",
+		AllowCredentials: false,
+		MaxAge:           86400, // 24 hours
+	}))
 
 	app.Use(helmet.New())   // Security headers
 	app.Use(compress.New()) // Response compression
@@ -57,10 +57,10 @@ func StartServer(app *fiber.App, config *config.Config) {
 	host := config.Env.ServerHost
 
 	serverAddr := host + ":" + port
-	log.Printf("Starting server on %s in %s mode", serverAddr, config.Env.Environment)
+	config.Logger.Info("Starting server", zap.String("address", serverAddr), zap.String("environment", config.Env.Environment))
 
 	// Start server
 	if err := app.Listen(serverAddr); err != nil {
-		log.Fatalf("Error starting server: %v", err)
+		config.Logger.Fatal("Error starting server", zap.Error(err))
 	}
 }
